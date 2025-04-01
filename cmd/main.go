f690922733615cdade1ad329ffde5b9624ddd394
goldioms <mharkunov@mail.ru>
2025-04-01 09:35:24 +0300

    feat: add database configuration and repository setup
    - Add .env to .gitignore to exclude environment variables
    - Include database configuration in config.yml
    - Add sqlx dependency and update repository to accept database connection
    - Implement NewPostgresDB function for database initialization
    - Update main.go to load environment variables and initialize database

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/goIdioms/rest"
 	"github.com/goIdioms/rest/pkg/handlers"
 	"github.com/goIdioms/rest/pkg/repository"
 	"github.com/goIdioms/rest/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	initConfig()
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config %s", err.Error())
+	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error load env variables %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(&repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handlers.NewHandler(services)
 
